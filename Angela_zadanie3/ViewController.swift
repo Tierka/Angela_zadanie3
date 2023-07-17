e1fd48a1c077215b8d78f8292343bd94d4106b66
Mariia Ovchinnikova <ghostlush@gmail.com>
2023-07-17 20:01:34 +0300

    Initial Commit

---
+++ Angela_zadanie3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Angela_zadanie3
+//
+//  Created by Maria on 17.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Angela_zadanie3UITests/Angela_zadanie3UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Angela_zadanie3UITestsLaunchTests.swift
+//  Angela_zadanie3UITests
+//
+//  Created by Maria on 17.07.2023.
+//
+
+import XCTest
+
+final class Angela_zadanie3UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
